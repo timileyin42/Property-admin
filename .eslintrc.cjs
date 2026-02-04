e9a7e523770bd2038af7160b1339ce446143ad44
timileyin42 <akanmuibro@gmail.com>
2026-02-04 18:39:25 +0100

    refactor: update ESLint rules to disable specific warnings for TypeScript and React

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,10 +10,9 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    '@typescript-eslint/no-explicit-any': 'off',
+    'react-hooks/exhaustive-deps': 'off',
+    'react-refresh/only-export-components': 'off',
   },
 }
 // 
